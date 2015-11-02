2b7bbed4bdb6373d8bc6c4e3d7bb6531b23d6d15
1304926 <1304926@rgu.ac.uk>
2015-11-02 14:45:21 +0000

    Test

---
+++ helloSelector.php
@@ -0,0 +1,6 @@
+<?php
+    echo "Where would you like to go?";
+    echo <a = href="http://workplz.azurewebsites.net/helloPrinter.php?query=Earth">Earth</a>;
+    echo <a = href="http://workplz.azurewebsites.net/helloPrinter.php?query=Mars">Mars</a>;
+    echo <a = href="http://workplz.azurewebsites.net/helloPrinter.php?query=Saturn">Saturn</a>;
+?>
