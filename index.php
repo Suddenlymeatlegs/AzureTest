49b308d54b036942924a32a298052fd5d82e3165
Suddenlymeatlegs <meatlegs1@gmail.com>
2015-11-02 11:46:07 +0000

    Update index.php

--- index.php
+++ index.php
@@ -5,13 +5,18 @@
     <body>
         <p>
             <?php
-                $provisionedActivity = array("Specs", "Drugs", "Rock and Roll");
-                foreach($provisionedActivity as $x){
-                    print "<p>$x</p>";
-                }
-                $provisionedActivity[0] = "Hugs";
-                foreach($provisionedActivity as $x){
-                    print "<p>$x</p>";
+                for($i; $i<30; $i++){
+                    $product = "No products";
+                    if($i % 4 == 0){
+                        $product = "Sausage rolls and specs";
+                    }
+                    elseif($i % 3 == 0){
+                        $product = "Mugs";
+                    }
+                    elseif($i % 2 == 0){
+                        $products = "Specs";
+                    }
+                    echo $product . " are available on the " . $i . "th day of the month."
                 }
             ?>
         </p>
